92bd2d865782c2eb9f053a225733fe06500b7e5c
LeCroissant <nico22330@gmail.com>
2014-11-21 14:51:41 +0100

    Supprime caractere avant cursor ok

--- src/main/scala/io/github/cr3ahal0/data/Buffer.scala
+++ src/main/scala/io/github/cr3ahal0/data/Buffer.scala
@@ -3,9 +3,9 @@ package data
 class Buffer {
 	
   private var text : String = ""
-  private var cursorPosition : Integer = 0
+  private var cursorPosition : Int = 0
   
-  def Buffer(text: String, cursorPosition: Integer){
+  def Buffer(text: String, cursorPosition: Int){
     this.text = text
     this.cursorPosition = cursorPosition
   }
@@ -18,29 +18,34 @@ class Buffer {
     this.text = text
   }
   
-  def setCursorPosition(position: Integer){
+  def setCursorPosition(position: Int){
     this.cursorPosition = position
   }
   
+  def write(char : Char)
+  {
+    text += char
+  }
+  
   //Add a string to the buffer at a position
-  def addStringAt(string: String, cursorPosition: Integer)
+  def addStringAt(string: String, cursorPosition: Int)
   {
     text = text.substring(0, cursorPosition)+string+text.substring(cursorPosition, text.length());
   }
   
   
   //Add a char to the buffer at a position
-  def addCharAt(char: Char, cursorPosition: Integer)
+  def addCharAt(char: Char, cursorPosition: Int)
   {
     text = text.substring(0, cursorPosition)+char+text.substring(cursorPosition, text.length());
   }
   
   //Remove the character previous the cursor position
-  def removePreviousChar(cursorPosition: Integer)
+  def removePreviousChar(cursorPosition: Int)
   {
-    text.substring(0, cursorPosition)+text.substring(cursorPosition+1, text.length())
+    text = text.substring(0, cursorPosition)+text.substring(cursorPosition+1, text.length())
   }
   
-  
+
   
 }
