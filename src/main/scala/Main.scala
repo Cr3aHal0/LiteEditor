2f7369adfb2a3e4568bcfd9e0c47147d86a3f210
LeCroissant <nico22330@gmail.com>
2014-11-21 15:37:47 +0100

    Editor menu 1->5 ok

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,19 @@
 import data.Buffer
+import data.Clipboard
+import command.Copy
+import command.Cut
+import command.Paste
+
 object Main {
 
 
 
 	var buffer : Buffer = new Buffer
+  var clipboard : Clipboard = new Clipboard
 	var choix : Int = 0
+  
 
-	/*Tant que l'utilisateur ne quitte pas le menu il reste dedans (6)*/
+	/*Tant que l'utilisateur ne quitte pas le menu (6) il reste dedans)*/
 
 
 	def menu()
@@ -16,6 +23,8 @@ object Main {
     { 
 		  while(choix < 1  || choix > 6)
 		  { 
+        println()
+        println()
   			println("Bienvenue dans le projet scala LiteEditor")
   			println("Veuillez choisir une option du menu")
   			println("[1] Ajouter une lettre au buffer")
@@ -28,17 +37,19 @@ object Main {
   			choix = readInt()
 		  }
 
-		  var char : Char = ' '
+		  var string : String = ""
 		  var position : Int = 0
+      var positionDebut : Int = 0
+      var positionFin : Int = 0
 		  choix match
 		  {
 
 			
 			
-  			case 1 => println("Donnez le caractere a ajouter au buffer")
-  			println("Entrez une lettre à ajouter au buffer : ")
-  			char = readChar
-  			buffer.write(char)
+  			case 1 => 
+  			print("Entrez une lettre ou phrase à ajouter au buffer : ")
+  			string = readLine()
+  			buffer.write(string)
   			println(buffer.getText)
         choix = 0
   			
@@ -48,6 +59,36 @@ object Main {
   			println(buffer.getText)
         choix = 0
   
+        case 3 => println("Position du curseur debut :")
+        positionDebut = readInt
+        println("Position du curseur fin :")        
+        positionFin = readInt
+        var copy : Copy = new Copy(buffer, clipboard, positionDebut, positionFin)
+        copy.execute()
+        choix = 0
+        
+        case 3 => println("Position du curseur debut :")
+        positionDebut = readInt
+        println("Position du curseur fin :")        
+        positionFin = readInt
+        var copy : Copy = new Copy(buffer, clipboard, positionDebut, positionFin)
+        copy.execute()
+        choix = 0
+        
+        case 4 => println("Position du curseur debut :")
+        positionDebut = readInt
+        println("Position du curseur fin :")        
+        positionFin = readInt
+        var cut : Cut = new Cut(buffer, clipboard, positionDebut, positionFin)
+        cut.execute()
+        choix = 0
+          
+        case 5 => println("Position du curseur : ")
+        position = readInt
+        var paste : Paste = new Paste(buffer, clipboard, position)
+        paste.execute()
+        choix = 0
+        
   			case 6 => println("***************** FIN DU PROGRAMME! *****************")
 
 
