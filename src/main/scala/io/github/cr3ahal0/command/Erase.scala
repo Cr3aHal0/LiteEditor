eb3f2b954b6b14fe12c703716d38b9b20d8f97dd
LeCroissant <nico22330@gmail.com>
2014-12-03 14:22:00 +0100

    Correction bug suppression

--- src/main/scala/io/github/cr3ahal0/command/Erase.scala
+++ src/main/scala/io/github/cr3ahal0/command/Erase.scala
@@ -2,14 +2,39 @@ package command
 import data.Buffer
 import data.BufferHistory
 import data.Clipboard
+import data.Selection
+import data.Cursor
 
-
-class Erase(buffer: Buffer) extends Command
+//Default : delete the previous char from the cursor position
+class Erase(buffer: Buffer, cursorPosition : Int) extends Command
 {
-	override def execute
-	{
+	
+  //Erase the selection in the buffer
+  def Erase(buffer: Buffer, selection: Selection)
+  {
+    buffer.setText("toto")
+    BufferHistory.addBuffer(buffer)
+  }
   
-	  buffer.setText(buffer.getText.substring(0, buffer.getCursorPosition)+buffer.getText.substring(buffer.getCursorPosition+1, buffer.getText.length()))
+  override def execute
+	{
+    //Delete the char previous the cursor position
+	  if(buffer.getLength <= 1)
+    {
+      buffer.setText("")
+      println("Impossible de supprimer, Buffer vide")
+    }
+    if(buffer.getCursorPosition <= 0){
+      println("Impossible de supprimer le caractere précédent, position du curseur = 0")
+    }
+    else
+    {
+         buffer.setText(buffer.getText.substring(0, cursorPosition-1)+buffer.getText.substring(cursorPosition, buffer.getText.length()))
+         //On deplace le curseur a la position -1 car il y a eu suppression de caracteres
+         buffer.setCursorPosition(cursorPosition-1)
+         println(buffer.getText)    
+    }
+   
     BufferHistory.addBuffer(buffer)
 	}
 }
