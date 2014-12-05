bf7f9500d3797cea6f2e8b019944acd7f48a1509
LeCroissant <nico22330@gmail.com>
2014-12-05 10:38:02 +0100

    Ajout macro

---
+++ src/main/scala/io/github/cr3ahal0/command/MacroClearAllBuffer.scala
@@ -0,0 +1,17 @@
+package command
+import data.Buffer
+import data.Selection
+
+class MacroClearAllBuffer(buffer : Buffer) extends Command{
+
+  
+  override def execute
+  {
+    //Selectionne tout le buffer
+    
+    //Delete le buffer avec la commande erase
+    
+  }
+  
+  
+}
